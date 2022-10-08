105c6e251eac233e94f297065048840213283b7b
Nathan Campos <nathan@innoveworkshop.com>
2022-10-08 10:12:18 +0100

    #24 - Started creating primitive object definitions

---
+++ engine/primitives.c
@@ -0,0 +1,117 @@
+/**
+ * engine/primitives.c
+ * Handles all of the operations related to the CAD engine's primitive objects.
+ *
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+#include "primitives.h"
+#include <stdio.h>
+
+/**
+ * Initializes a brand new primitive object structure.
+ * 
+ * @param obj primitive object structure to be initialized.
+ * @return ENGINE_OK if the operation was successful.
+ */
+engine_error_t primitive_new(primitive_obj_t *obj) {
+	obj->type = PRIMITIVE_TYPE_INVALID;
+	obj->layer = 0;
+	obj->coords = NULL;
+
+	return ENGINE_OK;
+}
+
+/**
+ * Frees up any resources allocated by the primitive object structure and
+ * invalidates it.
+ * 
+ * @param obj Primitive object structure to be free'd.
+ * @return ENGINE_OK if the operation was successful.
+ */
+engine_error_t primitive_free(primitive_obj_t *obj) {
+	/* Invalidate the object. */
+	obj->type = PRIMITIVE_TYPE_INVALID;
+	obj->layer = 0;
+
+	/* Free up the coordinates vector. */
+	cvector_free(obj->coords);
+	obj->coords = NULL;
+
+	return ENGINE_OK;
+}
+
+/**
+ * Create a coordinate structure from X and Y values.
+ * 
+ * @param x X position.
+ * @param y Y position.
+ *
+ * @return Pre-populated coordinate structure.
+ */
+coord_t coord_create(long x, long y) {
+	coord_t coord;
+
+	coord.x = x;
+	coord.y = y;
+
+	return coord;
+}
+
+/**
+ * Sets the primitive object type.
+ * 
+ * @param obj  Primitive object to be altered.
+ * @param type New primitive object type.
+ *
+ * @return ENGINE_OK if the operation was successful.
+ */
+engine_error_t primitive_set_type(primitive_obj_t *obj, primitive_type_t type) {
+	obj->type = type;
+	return ENGINE_OK;
+}
+
+/**
+ * Sets the primitive object layer.
+ * 
+ * @param obj   Primitive object to be altered.
+ * @param layer New primitive object layer.
+ *
+ * @return ENGINE_OK if the operation was successful.
+ */
+engine_error_t primitive_set_layer(primitive_obj_t *obj, uint8_t layer) {
+	obj->layer = layer;
+	return ENGINE_OK;
+}
+
+/**
+ * Appends a coordinate to the primitive object structure.
+ * 
+ * @param obj   Primitive object to be altered.
+ * @param coord New coordinate to be appended to the primitive object.
+ *
+ * @return ENGINE_OK if the operation was successful.
+ */
+engine_error_t primitive_add_coord(primitive_obj_t *obj, coord_t coord) {
+	cvector_push_back(obj->coords, coord);
+	return ENGINE_OK;
+}
+
+/**
+ * Dumps the contents of a primitive object to STDOUT as a JSON object.
+ * 
+ * @param obj Primitive object to be inspected.
+ */
+void primitive_debug_print(const primitive_obj_t *obj) {
+	/* I know this looks absolutely horrible. Stop staring at it! */
+	printf(
+		"{\n"
+		"    \"type\": %u,\n"
+		"    \"layer\": %u,\n"
+		"    \"coords\": [\n",
+		obj->type, obj->layer);
+
+/*		"        \"r\": %u,\n" */
+
+	printf("    ]\n}\n");
+}
---
+++ engine/primitives.h
@@ -0,0 +1,64 @@
+/**
+ * engine/primitives.h
+ * Handles all of the operations related to the CAD engine's primitive objects.
+ *
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+#ifndef _ENGINE_PRIMITIVES_H
+#define _ENGINE_PRIMITIVES_H
+
+#ifdef __cplusplus
+extern “C” {
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <cvector.h>
+
+#include "commons.h"
+
+/* Primitive object type definitions. */
+typedef enum {
+	PRIMITIVE_TYPE_INVALID = 0,
+	PRIMITIVE_TYPE_LINE,
+	PRIMITIVE_TYPE_RECT,
+	PRIMITIVE_TYPE_CIRCLE
+} primitive_type_t;
+
+/* Coordinate structure. */
+typedef struct {
+	long x;
+	long y;
+} coord_t;
+
+/* Primitive object structure. */
+typedef struct {
+	primitive_type_t type;
+	uint8_t layer;
+	cvector_vector_type(coord_t) coords;
+} primitive_obj_t;
+
+/* Constructor and Destructor */
+SHARED_API engine_error_t primitive_new(primitive_obj_t *obj);
+SHARED_API engine_error_t primitive_free(primitive_obj_t *obj);
+
+/* Convenience Functions for Coordinates */
+SHARED_API coord_t coord_create(long x, long y);
+
+/* Setters */
+SHARED_API engine_error_t primitive_set_type(primitive_obj_t *obj,
+	primitive_type_t type);
+SHARED_API engine_error_t primitive_set_layer(primitive_obj_t *obj,
+	uint8_t layer);
+SHARED_API engine_error_t primitive_add_coord(primitive_obj_t *obj,
+	coord_t coord);
+
+/* Debugging */
+SHARED_API void primitive_debug_print(const primitive_obj_t *obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _ENGINE_PRIMITIVES_H */
